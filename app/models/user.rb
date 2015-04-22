28a26ca74a9cb72a44ebf4868fe0ca25739365bd
wutuobangduan <wutuobangduan@163.com>
2015-04-22 14:30:31 +0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
 
-  before_save { self.email = email.downcase }
+  # before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 30 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # this REGEX can not refuse "foo@bar..com"
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates(:email, presence: true, format:{ with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } )
   validates :password, length: { minimum: 6 }
   has_secure_password
